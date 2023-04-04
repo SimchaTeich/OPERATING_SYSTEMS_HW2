71ef22a5333fafca7097006102049e388f4c0be7
97253 <sygw6457@gmail.com>
2023-04-04 19:56:14 +0300

    check codecB

--- codecB.c
+++ codecB.c
@@ -5,6 +5,12 @@
 
 void codecB(char *str)
 {
-    
-
+   int length = strlen(str);
+    //  each character in the string add 3 and if its out of range 
+    for (int i = 0; i < length; i++) 
+    {
+        // Add 3 to the ASCII value of the current character
+        // we calculate with modulo didnt be out of range 
+        str[i] = (str[i] + 3)%256;
+    }
 }
