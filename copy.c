bbb3c60dfbd1b0a234e7c0cde6eca03c0313f1c3
SimchaTeich <sim.teich@gmail.com>
2023-04-04 12:13:52 +0300

    create verboseIsOn function

--- copy.c
+++ copy.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 #define USAGE "usage: <file1> <file2> [-v] [-f]"
+#define VERBOSE_FLAG "-v"
+#define FORCE_FLAG "-f"
 #define MIN_PARAMETERS 3 // ./copy <file1> <file2>
+#define TRUE 1
+#define FALSE !TRUE
+
+// globals
+int verbose = FALSE;
+int force = FALSE;
+
+
+int verboseIsOn(const char* flag);
 
 
 int main(int argc, char** argv)
@@ -12,5 +24,27 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    if(argc > MIN_PARAMETERS)
+    {
+        verbose = verboseIsOn(argv[3]);
+        printf("verbose is %s\n", verbose ? "on" : "off");
+    }
     return 0;
+}
+
+
+/*
+check if char* is the '-v' flag
+input: flag from main parameters.
+output: 1 if it '-v', else 0.
+*/
+int verboseIsOn(const char* flag)
+{
+    if(strlen(VERBOSE_FLAG) == strlen(flag) &&
+    strcmp(VERBOSE_FLAG, flag) == 0)
+    {
+        return TRUE;
+    }
+
+    return FALSE;
 }
