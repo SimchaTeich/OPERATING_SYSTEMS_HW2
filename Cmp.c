a3a4816cb2517d867904a45028cffa31ab78f75a
97253 <sygw6457@gmail.com>
2023-04-04 10:58:35 +0300

    working on main section :)

--- Cmp.c
+++ Cmp.c
@@ -8,10 +8,18 @@
 
 
 int ignorCase(char a,char b);
+int compare_tow_BFiles(FILE *f1,FILE *f2 )
+
 
 
 int main(int argc ,char *argv[])
 {
+
+
+
+
+
+
 
 return EXIT_SUCCESS;
 }
@@ -25,15 +33,27 @@ int ignorCase(char a,char b)
         return -1;
 }
 
-void compare_tow_BFiles(FILE *f1,FILE *f2 )
+int compare_tow_BFiles(FILE *f1,FILE *f2 )
 {
    char ch0,ch1;
+   int check;
 
     while ((ch0=fgetc(f1))!=EOF  && (ch1=fgetc(f2))!=EOF)
     {
+            if((ch0==ch1) || (ignorCase(ch0, ch1)==1))//here i don't know if there need
+                                                            // of ignorCase option  check it
+            {
+                // if equal just continue until arrive to end of file
+                check =1;
+                continue;
+            } else
+            {
+                // if not equal shut down and stop while loop
+                fseek(f1,-1,SEEK_CUR);
+                check =0;
+                break;
 
+            }
     }
-
-
-
+    return check;
 }
