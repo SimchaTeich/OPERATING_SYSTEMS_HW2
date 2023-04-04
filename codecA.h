55471c82dd2bbcb36225b32e39ed22dcca49c3c3
SimchaTeich <sim.teich@gmail.com>
2023-04-04 15:55:36 +0300

    append codecA.h for libary

---
+++ codecA.h
@@ -0,0 +1,27 @@
+#define TRUE 1
+#define FALSE !TRUE
+
+
+/*
+convert all lower case to upper case,
+and all upper case to lower case.
+input: string as char*
+output: 0 for success, else 1
+*/
+int codecA(char* str);
+
+
+/*
+convert char to be lower case
+input: char
+output: same char in lower case
+*/
+char lower(char c);
+
+
+/*
+convert char to be upper case
+input: char
+output: same char in upper case
+*/
+char upper(char c);
