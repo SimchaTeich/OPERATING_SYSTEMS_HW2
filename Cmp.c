b2e8fa36f58b351e6d46f52eb269acff9d3138fc
97253 <sygw6457@gmail.com>
2023-04-04 11:23:00 +0300

    -at now didn't check anything upon linux ,  i will check and fix after I'm done of implement basic logic -isn't done.

--- Cmp.c
+++ Cmp.c
@@ -7,23 +7,38 @@
 #include <unistd.h>
 
 
-int ignorCase(char a,char b);
-int compare_tow_BFiles(FILE *f1,FILE *f2 )
+int ignoreCase(char a,char b);
+int compare_tow_BFiles(FILE* f1,FILE* f2 );
 
 
 
 int main(int argc ,char *argv[])
 {
 
+    int result;
+    File* file
+    File* file1;
+    // <filename1> <filename2> flag
+    file = fopen(argv[0],"r");
+    if (file == NULL)
+    {
+        printf("\nError in opening file or file doesnt exist %s", argv[0]);
+        return;
+    }
+    file1 = fopen(argv[1],"r");
+    if (file1 == NULL)
+    {
+        printf("\nError in opening file or file doesnt exist %s", argv[1]);
+        return;
+    }
+    if((file!=NULL)&&(file1!=NULL))
+    {
 
-
-
-
-
-
+        result = compare_tow_BFiles(file,file1);
+    }
 return EXIT_SUCCESS;
 }
-int ignorCase(char a,char b)
+int ignoreCase(char a,char b)
 {
     int value =abs(a-b);
     if(value==32)
@@ -33,15 +48,16 @@ int ignorCase(char a,char b)
         return -1;
 }
 
-int compare_tow_BFiles(FILE *f1,FILE *f2 )
+int compare_tow_BFiles(FILE* f1,FILE* f2 )
 {
    char ch0,ch1;
    int check;
 
     while ((ch0=fgetc(f1))!=EOF  && (ch1=fgetc(f2))!=EOF)
     {
-            if((ch0==ch1) || (ignorCase(ch0, ch1)==1))//here i don't know if there need
-                                                            // of ignorCase option  check it
+        // in this condition need to think again if can to use "ignoreCase"
+       // just need define "-i" flag or don't care about this
+            if((ch0==ch1) || (ignoreCase(ch0, ch1)==1))
             {
                 // if equal just continue until arrive to end of file
                 check =1;
