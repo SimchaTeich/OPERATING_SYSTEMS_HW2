7b6548b66c4c35e1b4dff9f4444c8fb6ee7c58d4
97253 <sygw6457@gmail.com>
2023-04-19 21:42:22 +0300

    small change to work well !

--- cmp.c
+++ cmp.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <unistd.h>
 
-
 #define USAGE "usage: <file1> <file2> [-v] [-i]"
 #define MIN_PARAMETERS 3 // ./copy <file1> <file2>
 #define VERBOSE_FLAG "-v"
@@ -15,7 +14,6 @@
 #define FALSE !TRUE
 #define NUM_OF_PARAMS_WITH_ONE_FLAGS 4
 #define NUM_OF_PARAMS_WITH_TWO_FLAGS 5
-
 
 // globals
 int verbose = FALSE;
@@ -26,6 +24,7 @@ int compare_tow_Icase_BFiles(FILE *f1, FILE *f2);
 int compare_tow_Vcase_BFiles(FILE *f1, FILE *f2);
 int flagIsOn(int argc, char **argv, const char *flag);
 int stringsAreEquals(const char *str1, const char *str2);
+int two_cases(int v, int i);
 
 int main(int argc, char *argv[])
 {
@@ -58,6 +57,21 @@ int main(int argc, char *argv[])
         // update globals about the flag status.
         verbose = flagIsOn(argc, argv, VERBOSE_FLAG);
         ignore = flagIsOn(argc, argv, IGNORE_FLAG);
+
+        if (two_cases(ignore, verbose) == 1)
+        {
+            if (compare_tow_Icase_BFiles(file, file1) == 1)
+            {
+                printf("\n equal \n");
+                return 1;
+            }
+            else
+            {
+                printf("\n distinct \n");
+                return 0;
+            }
+        }
+
         if (verbose)
         {
             if (compare_tow_Vcase_BFiles(file, file1) == 1)
@@ -88,7 +102,6 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-
 /*
  * “ignore case” so “AAA” and “aaa” meaning equals
  */
@@ -106,7 +119,6 @@ int ignoreCase(char a, char b)
     return -1;
 }
 
-
 int compare_tow_Icase_BFiles(FILE *f1, FILE *f2)
 {
     char ch0, ch1;
@@ -114,7 +126,7 @@ int compare_tow_Icase_BFiles(FILE *f1, FILE *f2)
 
     while (((ch0 = fgetc(f1)) != EOF) && ((ch1 = fgetc(f2)) != EOF))
     {
-        // just need define "-i" 
+        // just need define "-i"
         if ((ch0 == ch1) || (ignoreCase(ch0, ch1) == 1))
         {
             // if equal just continue until arrive to end of file
@@ -159,7 +171,6 @@ int compare_tow_Vcase_BFiles(FILE *f1, FILE *f2)
     return check;
 }
 
-
 /*
 check if two strings (char*) are equals
 input: two char*
@@ -175,7 +186,6 @@ int stringsAreEquals(const char *str1, const char *str2)
 
     return FALSE;
 }
-
 
 /*
 check if at least one argument is a flag.
@@ -194,4 +204,16 @@ int flagIsOn(int argc, char **argv, const char *flag)
     }
 
     return FALSE;
+}
+
+// to define the case e have two pararmiters (i, v)
+int two_cases(int v, int i)
+{
+
+    int result = v + i;
+    if (result == 2)
+    {
+        return 1;
+    }
+    return 0;
 }
