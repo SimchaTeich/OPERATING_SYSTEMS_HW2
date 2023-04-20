a4baaf55aee948c8f0e4de3ae253d36bf4711413
97253 <sygw6457@gmail.com>
2023-04-20 12:15:06 +0300

    hmmmmmm...

--- cmp.c
+++ cmp.c
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
             }
             else
             {
-                printf("\n distinct \n");
+                printf(" distinct \n");
                 return 0;
             }
         }
